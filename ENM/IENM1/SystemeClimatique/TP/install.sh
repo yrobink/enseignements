4802e66016b9a1cbfa3d112757ea6b52c6b664d7
ROBIN Yoann <yoann.robin.k@gmail.com>
2020-01-07 07:58:24 +0100

    First commit

---
+++ ENM/IENM1/SystemeClimatique/TP/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pip3 install --user numpy==1.17.5
+pip3 install --user scipy
+pip3 install --user pandas
+pip3 install --user xarray=0.13.0
+pip3 install --user matplotlib
+pip3 install --user cftime=1.0.3.4
+#pip3 install --user cartopy
+#pip3 install --user jupyterlab
